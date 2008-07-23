d23eac162d0a8c37d4f3a193400898bcbe3dd6da
Pedro Belo <pedro@bitscribe.net>
2008-07-23 15:09:15 -0700

    add spec for client.update

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -36,6 +36,12 @@ EOXML
 		@client.create("newapp").should == "newapp"
 	end
 
+	it "update(name, attributes) -> updates existing apps" do
+		@client.should_receive(:resource).with('/apps/myapp').and_return(@resource)
+		@resource.should_receive(:put).with({ 'app[mode]' => 'production', 'app[public]' => true }, anything)
+		@client.update("myapp", :mode => 'production', :public => true)
+	end
+
 	it "destroy(name) -> destroy the named app" do
 		@client.should_receive(:resource).with('/apps/destroyme').and_return(@resource)
 		@resource.should_receive(:delete)
