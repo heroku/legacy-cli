96625e77a341caeb563ab86b3d2f0a20c384a69d
Pedro Belo <pedro@heroku.com>
2008-11-08 15:31:40 -0800

    added app commands

---
+++ lib/heroku/commands/app.rb
@@ -0,0 +1,110 @@
+require 'readline'
+
+module Heroku::Command
+	class App < Base
+		def list
+			list = heroku.list
+			if list.size > 0
+				display list.join("\n")
+			else
+				display "You have no apps."
+			end
+		end
+
+		def create
+			name = args.shift.downcase.strip rescue nil
+			name = heroku.create(name, {})
+			display "Created #{app_urls(name)}"
+		end
+
+		def rename
+			name = extract_app(args)
+			newname = args.shift.downcase.strip rescue ''
+			raise(CommandFailed, "Invalid name.") if newname == ''
+
+			heroku.update(name, :name => newname)
+
+			display app_urls(newname)
+			display "Don't forget to update your Git remotes on any local checkouts."
+		end
+
+		def info
+			name = extract_app(args)
+			attrs = heroku.info(name)
+			display "=== #{attrs[:name]}"
+			display "Web URL:        http://#{attrs[:name]}.#{heroku.host}/"
+			display "Domain name:    http://#{attrs[:domain_name]}/" if attrs[:domain_name]
+			display "Git Repo:       git@#{heroku.host}:#{attrs[:name]}.git"
+			display "Mode:           #{ attrs[:production] == 'true' ? 'production' : 'development' }"
+			display "Code size:      #{format_bytes(attrs[:code_size])}" if attrs[:code_size]
+			display "Data size:      #{format_bytes(attrs[:data_size])}" if attrs[:data_size]
+			display "Public:         #{ attrs[:share_public] == 'true' ? 'true' : 'false' }"
+
+			first = true
+			lead = "Collaborators:"
+			attrs[:collaborators].each do |collaborator|
+				display "#{first ? lead : ' ' * lead.length}  #{collaborator[:email]} (#{collaborator[:access]})"
+				first = false
+			end
+		end
+
+		def rake
+			app = extract_app
+			cmd = args.join(' ')
+			if cmd.length == 0
+				display "Usage: heroku rake <command>"
+			else
+				display heroku.rake(app, cmd)
+			end
+		end
+
+		def console
+			app = extract_app
+			cmd = args.join(' ').strip
+			if cmd.empty?
+				console_session
+			else
+				display heroku.console(app, cmd)
+			end
+		end
+
+		def console_session
+			app = extract_app
+			display "Ruby console for #{app}.#{heroku.host}"
+			heroku.console(app) do |console|
+				while cmd = Readline.readline('>> ')
+					break if cmd.downcase.strip == 'exit'
+					unless cmd.nil? || cmd.strip.empty?
+						Readline::HISTORY.push(cmd)
+						display console.run(cmd)
+					end
+				end
+			end
+		end
+
+		def restart
+			app_name = extract_app
+			heroku.restart(app_name)
+			display "Servers restarted"
+		end
+
+		def logs
+			app_name = extract_app
+			display heroku.logs(app_name)
+		end
+
+		protected
+			@@kb = 1024
+			@@mb = 1024 * @@kb
+			@@gb = 1024 * @@mb
+			def format_bytes(amount)
+				amount = amount.to_i
+				return nil if amount == 0
+				return amount if amount < @@kb
+				return "#{(amount / @@kb).round}k" if amount < @@mb
+				return "#{(amount / @@mb).round}M" if amount < @@gb
+				return "#{(amount / @@gb).round}G"
+			end
+
+	end
+end
---
+++ spec/commands/app_spec.rb
@@ -0,0 +1,65 @@
+require File.dirname(__FILE__) + '/../base'
+
+module Heroku::Command
+	describe App do
+		before do
+			@cli = prepare_command(App)
+		end
+
+		it "shows app info, converting bytes to kbs/mbs" do
+			@cli.heroku.should_receive(:info).with('myapp').and_return({ :name => 'myapp', :collaborators => [], :code_size => 2*1024, :data_size => 5*1024*1024 })
+			@cli.should_receive(:display).with('=== myapp')
+			@cli.should_receive(:display).with('Web URL:        http://myapp.heroku.com/')
+			@cli.should_receive(:display).with('Code size:      2k')
+			@cli.should_receive(:display).with('Data size:      5M')
+			@cli.info
+		end
+
+		it "creates without a name" do
+			@cli.heroku.should_receive(:create).with(nil, {}).and_return("untitled-123")
+			@cli.create
+		end
+
+		it "creates with a name" do
+			@cli.stub!(:args).and_return([ 'myapp' ])
+			@cli.heroku.should_receive(:create).with('myapp', {}).and_return("myapp")
+			@cli.create
+		end
+
+		it "renames an app" do
+			@cli.stub!(:args).and_return([ 'myapp2' ])
+			@cli.heroku.should_receive(:update).with('myapp', { :name => 'myapp2' })
+			@cli.rename
+		end
+
+		it "runs a rake command on the app" do
+			@cli.stub!(:args).and_return(([ 'db:migrate' ]))
+			@cli.heroku.should_receive(:rake).with('myapp', 'db:migrate')
+			@cli.rake
+		end
+
+		it "runs a single console command on the app" do
+			@cli.stub!(:args).and_return([ '2+2' ])
+			@cli.heroku.should_receive(:console).with('myapp', '2+2')
+			@cli.console
+		end
+
+		it "offers a console, opening and closing the session with the client" do
+			@console = mock('heroku console')
+			@cli.heroku.should_receive(:console).with('myapp').and_yield(@console)
+			Readline.should_receive(:readline).and_return('exit')
+			@cli.console
+		end
+
+		it "asks to restart servers" do
+			@cli.heroku.should_receive(:restart).with('myapp')
+			@cli.restart
+		end
+
+		it "shows the app logs" do
+			@cli.heroku.should_receive(:logs).with('myapp').and_return('logs')
+			@cli.should_receive(:display).with('logs')
+			@cli.logs
+		end
+	end
+end
