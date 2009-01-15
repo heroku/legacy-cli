650993b88c2e047b518e9c699b6a7c5143d5e422
Adam Wiggins <adam@heroku.com>
2009-01-15 12:56:10 -0800

    fix sharing:add

--- lib/heroku/commands/sharing.rb
+++ lib/heroku/commands/sharing.rb
@@ -7,12 +7,14 @@ module Heroku::Command
 		alias :index :list
 
 		def add
-			email = args.shift.downcase rescue nil
-			display heroku.add_collaborator(app, email, 'edit')
+			email = args.shift.downcase rescue ''
+			raise(CommandFailed, "Specify an email address to share the app with.") if email == ''
+			display heroku.add_collaborator(app, email)
 		end
 
 		def remove
-			email = args.shift.downcase rescue nil
+			email = args.shift.downcase rescue ''
+			raise(CommandFailed, "Specify an email address to remove from the app.") if email == ''
 			heroku.remove_collaborator(app, email)
 			display "Collaborator removed."
 		end
