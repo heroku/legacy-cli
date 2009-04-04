996352a95652976ee486712cc60b9c6844f9acee
Adam Wiggins <adam@crescent.(none)>
2009-04-04 15:47:14 -0700

    more consistent display of config vars on add

--- lib/heroku/commands/config.rb
+++ lib/heroku/commands/config.rb
@@ -3,11 +3,11 @@ module Heroku::Command
 		def index
 			long = args.delete('--long')
 			vars = heroku.config_vars(app)
-			display_vars(vars, long)
+			display_vars(vars, :long => long)
 		end
 
 		def add
-			unless args.all? { |a| a.include?('=') }
+			unless args.size > 0 and args.all? { |a| a.include?('=') }
 				raise CommandFailed, "Usage: heroku config:add <key>=<value> [<key2>=<value2> ...]"
 			end
 
@@ -17,40 +17,37 @@ module Heroku::Command
 				vars
 			end
 
-			display "Setting #{vars.inspect} and restarting app...", false
+			display "Adding config vars:"
+			display_vars(vars, :indent => 2)
+
+			display "Restarting app...", false
 			heroku.add_config_vars(app, vars)
 			display "done."
 		end
 
 		def remove
-			display "Unsetting #{args.first} and restarting app...", false
+			display "Removing #{args.first} and restarting app...", false
 			heroku.remove_config_var(app, args.first)
 			display "done."
 		end
 
 		def clear
-			display "Reseting all config vars and restarting app...", false
+			display "Clearing all config vars and restarting app...", false
 			heroku.clear_config_vars(app)
 			display "done."
 		end
 
 		protected
-			def restart_app
-				display "Restarting app...", false
-				heroku.restart(app)
-				display "done."
-			end
-
-			def display_vars(vars, long)
+			def display_vars(vars, options={})
 				max_length = vars.map { |v| v[0].size }.max
 				vars.each do |k, v|
 					spaces = ' ' * (max_length - k.size)
-					display "#{k}#{spaces} => #{format(v, long)}"
+					display "#{' ' * (options[:indent] || 0)}#{k}#{spaces} => #{format(v, options)}"
 				end
 			end
 
-			def format(value, long=false)
-				return value if long || value.size < 36
+			def format(value, options)
+				return value if options[:long] || value.size < 36
 				value[0, 16] + '...' + value[-16, 16]
 			end
 	end
