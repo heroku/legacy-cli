74ea242e5053764ff84c7aa9351049210f91c793
Nick Quaranto <nick@quaran.to>
2009-09-30 16:36:54 -0400

    Being a little more verbose when using maintenance mode

--- lib/heroku/commands/maintenance.rb
+++ lib/heroku/commands/maintenance.rb
@@ -2,10 +2,12 @@ module Heroku::Command
 	class Maintenance < BaseWithApp
 		def on
 			heroku.maintenance(app, :on)
+			display "Maintenance mode enabled."
 		end
 
 		def off
 			heroku.maintenance(app, :off)
+			display "Maintenance mode disabled."
 		end
 	end
 end
--- spec/commands/maintenance_spec.rb
+++ spec/commands/maintenance_spec.rb
@@ -8,11 +8,13 @@ module Heroku::Command
 
 		it "turns on maintenance mode for the app" do
 			@m.heroku.should_receive(:maintenance).with('myapp', :on)
+			@m.should_receive(:display).with('Maintenance mode enabled.')
 			@m.on
 		end
 
 		it "turns off maintenance mode for the app" do
 			@m.heroku.should_receive(:maintenance).with('myapp', :off)
+			@m.should_receive(:display).with('Maintenance mode disabled.')
 			@m.off
 		end
 	end
