e541d4012b7f2d02e53e4c92e661486cf6f5b774
Pedro Belo <pedro@heroku.com>
2009-11-11 19:33:15 -0800

    enhance display of confirm_billing during addon installation

--- lib/heroku/commands/addons.rb
+++ lib/heroku/commands/addons.rb
@@ -64,8 +64,9 @@ module Heroku::Command
 		end
 
 		def confirm_billing
-			display("This action will cause your account to be billed at the end of the month")
-			display("Are you sure you want to do this? (y/n) ", false)
+			display("\n")
+			display("  This action will cause your account to be billed at the end of the month")
+			display("  Are you sure you want to do this? (y/n) ", false)
 			if ask.downcase == 'y'
 				heroku.confirm_billing
 				return true
