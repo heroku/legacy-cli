93fed190df158d4cf393639cb3977921972f5b5a
Pedro Belo <pedro@heroku.com>
2009-12-16 15:36:36 -0800

    exit with error when running deprecated service commands

--- lib/heroku/commands/service.rb
+++ lib/heroku/commands/service.rb
@@ -1,23 +1,23 @@
 module Heroku::Command
 	class Service < Base
 		def start
-			display "heroku service:start is defunct.  Use heroku workers +1 instead."
+			error "heroku service:start is defunct.  Use heroku workers +1 instead."
 		end
 
 		def up
-			display "heroku service:up is defunct.  Use heroku workers +1 instead."
+			error "heroku service:up is defunct.  Use heroku workers +1 instead."
 		end
 
 		def down
-			display "heroku service:start is defunct.  Use heroku workers -1 instead."
+			error "heroku service:down is defunct.  Use heroku workers -1 instead."
 		end
 
 		def bounce
-			display "heroku service:start is defunct.  Use heroku restart instead."
+			error "heroku service:bounce is defunct.  Use heroku restart instead."
 		end
 
 		def status
-			display "heroku service:status is defunct.  Use heroku ps instead."
+			error "heroku service:status is defunct.  Use heroku ps instead."
 		end
 	end
 end
