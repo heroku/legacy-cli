502e55b618fc45692e69ae908e9d72585d87287b
Pedro Belo <pedro@heroku.com>
2009-12-17 14:18:01 -0800

    cosmetics

--- lib/heroku/commands/addons.rb
+++ lib/heroku/commands/addons.rb
@@ -63,9 +63,11 @@ module Heroku::Command
 			end
 		end
 
+		# TODO merge with Account#confirm_billing
 		def confirm_billing
 			display("\n")
 			display("  This action will cause your account to be billed at the end of the month")
+			display("  For more information, see http://docs.heroku.com/billing")
 			display("  Are you sure you want to do this? (y/n) ", false)
 			if ask.downcase == 'y'
 				heroku.confirm_billing
