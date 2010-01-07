0b4cfb216d14e3a0bab2f085e702647f3c75c4b7
David Dollar <ddollar@gmail.com>
2010-01-07 09:40:49 -0500

    Revert "pin dependencies to a specific version during load"
    This reverts commit 7fdd3efebe22ed500d83dcb1b14677b98f42d09a.

--- lib/heroku.rb
+++ lib/heroku.rb
@@ -1,9 +1,5 @@
 module Heroku; end
 
-gem 'rest-client', '= 1.0.3'
-gem 'launchy',     '= 0.3.2'
-gem 'json',        '= 1.1.0'
-
 $LOAD_PATH.unshift(File.dirname(__FILE__) + '/heroku')
 
 require 'client'
