e20e1e907b091c8665b7e059d6e9418a18a6d373
David Dollar <ddollar@gmail.com>
2010-10-18 12:55:34 -0700

    bail out when installing heroku plugins that do not load

--- lib/heroku/commands/plugins.rb
+++ lib/heroku/commands/plugins.rb
@@ -10,6 +10,11 @@ module Heroku::Command
     def install
       plugin = Heroku::Plugin.new(args.shift)
       if plugin.install
+        begin
+          Heroku::Plugin.load_plugin(plugin.name)
+        rescue Exception => ex
+          installation_failed(plugin, ex.message)
+        end
         display "#{plugin} installed"
       else
         error "Could not install #{plugin}. Please check the URL and try again"
@@ -21,5 +26,22 @@ module Heroku::Command
       plugin.uninstall
       display "#{plugin} uninstalled"
     end
+
+    protected
+
+      def installation_failed(plugin, message)
+        plugin.uninstall
+        error <<-ERROR
+Could not initialize #{plugin}: #{message}
+
+Are you attempting to install a Rails plugin? If so, use the following:
+
+Rails 2.x:
+script/plugin install #{plugin.uri}
+
+Rails 3.x:
+rails plugin install #{plugin.uri}
+        ERROR
+      end
   end
 end
