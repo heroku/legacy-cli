d1fbb0a534cad60ebf5392edfda1440b631701f3
David Dollar <ddollar@gmail.com>
2010-11-21 22:58:45 -0500

    move common commands into helpers

--- lib/heroku/commands/base.rb
+++ lib/heroku/commands/base.rb
@@ -14,51 +14,6 @@ module Heroku::Command
       @autodetected_app = false
     end
 
-    def confirm(message="Are you sure you wish to continue? (y/n)?")
-      display("#{message} ", false)
-      ask.downcase == 'y'
-    end
-
-    def confirm_command(app = app)
-      if extract_option('--force')
-        display("Warning: The --force switch is deprecated, and will be removed in a future release. Use --confirm #{app} instead.")
-        return true
-      end
-
-      raise(CommandFailed, "No app specified.\nRun this command from app folder or set it adding --app <app name>") unless app
-
-      confirmed_app = extract_option('--confirm', false)
-      if confirmed_app
-        unless confirmed_app == app
-          raise(CommandFailed, "Confirmed app #{confirmed_app} did not match the selected app #{app}.")
-        end
-        return true
-      else
-        display "\n !    Potentially Destructive Action"
-        display " !    To proceed, type \"#{app}\" or re-run this command with --confirm #{@app}"
-        display "> ", false
-        if ask.downcase != app
-          display " !    Input did not match #{app}. Aborted."
-          false
-        else
-          true
-        end
-      end
-    end
-
-    def format_date(date)
-      date = Time.parse(date) if date.is_a?(String)
-      date.strftime("%Y-%m-%d %H:%M %Z")
-    end
-
-    def ask
-      gets.strip
-    end
-
-    def shell(cmd)
-      FileUtils.cd(Dir.pwd) {|d| return `#{cmd}`}
-    end
-
     def heroku
       @heroku ||= Heroku::Command.run_internal('auth:client', args)
     end
