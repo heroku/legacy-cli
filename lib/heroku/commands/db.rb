c1200c5f11006b71244645db6c55f178691a4ed8
David Dollar <ddollar@gmail.com>
2011-03-02 10:26:15 -0500

    bump taps requirement to 0.3.17

--- lib/heroku/commands/db.rb
+++ lib/heroku/commands/db.rb
@@ -162,7 +162,7 @@ module Heroku::Command
     def load_taps
       require 'taps/operation'
       require 'taps/cli'
-      enforce_taps_version "0.3.16"
+      enforce_taps_version "0.3.17"
       display "Loaded Taps v#{Taps.version}"
     rescue LoadError
       message  = "Taps Load Error: #{$!.message}\n"
