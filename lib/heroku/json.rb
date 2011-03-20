9f8b3f912a41f907b53eb2b3f7ab4d38a2a009f7
David Dollar <ddollar@gmail.com>
2011-03-20 16:46:55 -0400

    use activesupport for json

--- lib/heroku/json.rb
+++ lib/heroku/json.rb
@@ -1,5 +1,13 @@
+require "active_support"
+require "active_support/json"
+require "active_support/ordered_hash"
+
+# for compatibility with activesupport 2.2 and below
 unless Kernel.const_defined?(:JSON)
-  $:.unshift File.expand_path("../vendor/json_pure-1.5.1/lib", __FILE__)
-  require "json/pure"
+  module JSON
+    def self.parse(json)
+      ActiveSupport::JSON.decode(json)
+    end
+  end
 end
 
