400b5a2e966d76712a1b64a50aa9f8ae0d1df3c1
David Dollar <ddollar@gmail.com>
2011-04-11 14:12:10 -0400

    add help to ssl

--- lib/heroku/command/ssl.rb
+++ lib/heroku/command/ssl.rb
@@ -1,8 +1,16 @@
 require "heroku/command/base"
 
 module Heroku::Command
+
+  # manage ssl certificates for an app
+  #
   class Ssl < BaseWithApp
-    def list
+
+    # ssl
+    #
+    # list certificates for an app
+    #
+    def index
       heroku.list_domains(app).each do |d|
         if cert = d[:cert]
           display "#{d[:domain]} has a SSL certificate registered to #{cert[:subject]} which expires on #{cert[:expires_at].strftime("%b %d, %Y")}"
@@ -11,12 +19,14 @@ module Heroku::Command
         end
       end
     end
-    alias :index :list
 
+    # ssl:add PEM KEY
+    #
+    # add an ssl certificate to an app
+    #
     def add
-      usage  = 'heroku ssl:add <pem> <key>'
-      raise CommandFailed, "Missing pem file. Usage:\n#{usage}" unless pem_file = args.shift
-      raise CommandFailed, "Missing key file. Usage:\n#{usage}" unless key_file = args.shift
+      raise CommandFailed, "Missing pem file." unless pem_file = args.shift
+      raise CommandFailed, "Missing key file." unless key_file = args.shift
       raise CommandFailed, "Could not find pem in #{pem_file}"  unless File.exists?(pem_file)
       raise CommandFailed, "Could not find key in #{key_file}"  unless File.exists?(key_file)
 
@@ -26,12 +36,20 @@ module Heroku::Command
       display "Added certificate to #{info['domain']}, expiring in #{info['expires_at']}"
     end
 
+    # ssl:remove DOMAIN
+    #
+    # remove an ssl certificate from an app
+    #
     def remove
       raise CommandFailed, "Missing domain. Usage:\nheroku ssl:remove <domain>" unless domain = args.shift
       heroku.remove_ssl(app, domain)
       display "Removed certificate from #{domain}"
     end
 
+    # ssl:clear
+    #
+    # remove all ssl certificates from an app
+    #
     def clear
       heroku.clear_ssl(app)
       display "Cleared certificates for #{app}"
