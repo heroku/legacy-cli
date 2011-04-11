cf506fba3e87fa4454f6c2c767d7114bbf649c86
David Dollar <ddollar@gmail.com>
2011-04-11 11:50:31 -0400

    add help to maintenance

--- lib/heroku/command/maintenance.rb
+++ lib/heroku/command/maintenance.rb
@@ -1,12 +1,23 @@
 require "heroku/command/base"
 
 module Heroku::Command
+
+  # toggle maintenance mode
   class Maintenance < BaseWithApp
+
+    # maintenance:on
+    #
+    # put the app into maintenance mode
+    #
     def on
       heroku.maintenance(app, :on)
       display "Maintenance mode enabled."
     end
 
+    # maintenance:off
+    #
+    # take the app out of maintenance mode
+    #
     def off
       heroku.maintenance(app, :off)
       display "Maintenance mode disabled."
