3a1680e2e50e488f444a82173749cee498f222b2
Will Leinweber <will@bitfission.com>
2011-05-11 16:01:08 -0700

    send heroku gem version to pgbackups

--- spec/heroku/pgbackupsclient_spec.rb
+++ spec/heroku/pgbackupsclient_spec.rb
@@ -1,32 +1,41 @@
 require "spec_helper"
 require "heroku/helpers"
+require "heroku/command/pgbackups"
 require "pgbackups/client"
-
-def pgbackups_api_stub(method, path)
-  stub_request(method, "http://id:password@pgbackups.heroku.com#{path}")
-end
-
-def pgbackups_api_request(method, path)
-  a_request(method, "http://id:password@pgbackups.heroku.com#{path}")
-end
 
 describe PGBackups::Client do
   include Heroku::Helpers
+  let(:path)   { "http://id:password@pgbackups.heroku.com" }
+  let(:client) { PGBackups::Client.new path+'/api' }
+  let(:transfer_path) { path + '/client/transfers' }
 
-  before do
-    @client = PGBackups::Client.new("http://id:password@pgbackups.heroku.com/api")
+  describe "api" do
+    let(:version) { Heroku::Client.version }
+
+    it 'still has a heroku gem version' do
+      version.should be
+      version.split(/\./).first.to_i.should >= 2
+    end
+
+    it 'incldues the heroku gem version' do
+      stub_request(:get, transfer_path)
+      client.get_transfers
+      a_request(:get, transfer_path).with(
+        :headers => {'X-Heroku-Gem-Version' => version}
+      ).should have_been_made.once
+    end
   end
 
-  describe "transfers" do
+  describe "create transfers" do
     it "sends a request to the client" do
-      pgbackups_api_stub(:post, "/client/transfers").to_return(
+      stub_request(:post, transfer_path).to_return(
         :body => json_encode({:message => "success"}),
         :status => 200
       )
 
-      @client.create_transfer("postgres://from", "postgres://to", "FROMNAME", "TO_NAME")
+      client.create_transfer("postgres://from", "postgres://to", "FROMNAME", "TO_NAME")
 
-      pgbackups_api_request(:post, "/client/transfers").should have_been_made.once
+      a_request(:post, transfer_path).should have_been_made.once
     end
   end
 
