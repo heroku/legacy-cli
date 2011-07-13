3168e5594b8dac454d5999dfaee65cefe9ade951
Mark McGranaghan <mmcgrana@gmail.com>
2011-07-12 21:39:59 -0700

    rendezvous ssl only

--- lib/heroku/client/rendezvous.rb
+++ lib/heroku/client/rendezvous.rb
@@ -23,60 +23,33 @@ class Heroku::Client::Rendezvous
     uri = URI.parse(rendezvous_url)
     scheme, host, port, secret = uri.scheme, uri.host, uri.port, uri.path[1..-1]
 
-    if (scheme == "rendezvous")
-      tcp_socket, ssl_socket = Timeout.timeout(30) do
-        ssl_context = OpenSSL::SSL::SSLContext.new
-        if ((host =~ /heroku\.com$/) && !(ENV["HEROKU_SSL_VERIFY"] == "disable"))
-          ssl_context.ca_file = File.expand_path("../../../../data/cacert.pem", __FILE__)
-          ssl_context.verify_mode = OpenSSL::SSL::VERIFY_PEER
-        end
-        tcp_socket = TCPSocket.open(host, port)
-        ssl_socket = OpenSSL::SSL::SSLSocket.new(tcp_socket, ssl_context)
-        ssl_socket.connect
-        ssl_socket.puts(secret)
-        ssl_socket.readline
-        [tcp_socket, ssl_socket]
+    tcp_socket, ssl_socket = Timeout.timeout(30) do
+      ssl_context = OpenSSL::SSL::SSLContext.new
+      if ((host =~ /heroku\.com$/) && !(ENV["HEROKU_SSL_VERIFY"] == "disable"))
+        ssl_context.ca_file = File.expand_path("../../../../data/cacert.pem", __FILE__)
+        ssl_context.verify_mode = OpenSSL::SSL::VERIFY_PEER
       end
+      tcp_socket = TCPSocket.open(host, port)
+      ssl_socket = OpenSSL::SSL::SSLSocket.new(tcp_socket, ssl_context)
+      ssl_socket.connect
+      ssl_socket.puts(secret)
+      ssl_socket.readline
+      [tcp_socket, ssl_socket]
+    end
 
-      set_buffer(false)
-      output.sync = true
-      on_connect.call if on_connect
+    set_buffer(false)
+    output.sync = true
+    on_connect.call if on_connect
 
-      loop do
-        if o = IO.select([input, tcp_socket], nil, nil, nil)
-          if o.first.first == input
-            data = input.readpartial(1000)
-            ssl_socket.write(data)
-            ssl_socket.flush
-          elsif o.first.first == tcp_socket
-            data = ssl_socket.readpartial(1000)
-            output.write(data)
-          end
-        end
-      end
-
-    else
-      socket = Timeout.timeout(30) do
-        s = TCPSocket.open(host, port)
-        s.puts(secret)
-        s.readline; s.readline
-        s
-      end
-
-      set_buffer(false)
-      output.sync = true
-      on_connect.call if on_connect
-
-      loop do
-        if o = IO.select([input, socket], nil, nil, nil)
-          if o.first.first == input
-            data = input.read_nonblock(1000)
-            socket.write(data)
-            socket.flush()
-          elsif o.first.first == socket
-            data = socket.read_nonblock(1000)
-            output.write(data)
-          end
+    loop do
+      if o = IO.select([input, tcp_socket], nil, nil, nil)
+        if o.first.first == input
+          data = input.readpartial(1000)
+          ssl_socket.write(data)
+          ssl_socket.flush
+        elsif o.first.first == tcp_socket
+          data = ssl_socket.readpartial(1000)
+          output.write(data)
         end
       end
     end
