71d9dd19c8ddeeb0c8bbaa3575997839d6ea5feb
David Dollar <ddollar@gmail.com>
2012-01-09 16:04:05 -0800

    fix typo

--- lib/heroku/updater.rb
+++ lib/heroku/updater.rb
@@ -51,7 +51,7 @@ module Heroku
       $:.unshift File.join(updated_client_path, "lib")
       vendored_gems = Dir[File.join(updated_client_path, "vendor", "gems", "*")]
       vendored_gems.each do |vendored_gem|
-        $:.unshift File.join(vendored_gem, lib)
+        $:.unshift File.join(vendored_gem, "lib")
       end
     end
   end
