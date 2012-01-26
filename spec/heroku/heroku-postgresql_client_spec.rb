0715afc322a58ebdb6a07d717824fe9d46b4eb47
geemus <geemus@gmail.com>
2012-01-25 18:47:01 -0600

    test cleanup (get tests passing again locally)

--- spec/heroku/heroku-postgresql_client_spec.rb
+++ spec/heroku/heroku-postgresql_client_spec.rb
@@ -3,41 +3,31 @@ require "heroku/helpers"
 require "heroku-postgresql/client"
 require 'digest'
 
-def shogun_path(path)
-  "https://shogun.heroku.com/client/v10/#{path}"
-end
-
-def hk_pg_api_stub(method, path)
-  stub_request(method, shogun_path(path))
-end
-
-def hk_pg_api_request(method, path)
-  a_request(method, shogun_path(path))
-end
-
 describe HerokuPostgresql::Client do
   include Heroku::Helpers
   let(:url)     { 'postgres://somewhere/somedb' }
   let(:url_sha) { Digest::SHA2.hexdigest url }
   let(:client)  { HerokuPostgresql::Client.new(url) }
 
-
   it "sends an ingress request to the client" do
-    hk_pg_api_stub(:put, "databases/#{url_sha}/ingress").to_return(
+    url = "https://shogun.heroku.com/client/v10/databases/#{url_sha}/ingress"
+
+    stub_request(:put, url).to_return(
       :body => json_encode({:message => "ok"}),
       :status => 200
     )
 
     client.ingress
 
-    hk_pg_api_request(:put, "databases/#{url_sha}/ingress").should have_been_made.once
+    a_request(:put, url).should have_been_made.once
   end
 
   it "retries on error, then raises" do
-    hk_pg_api_stub(:get, "databases/#{url_sha}").to_return(:body => "error", :status => 500)
+    url = "https://shogun.heroku.com/client/v10/databases/#{url_sha}"
+    stub_request(:get, url).to_return(:body => "error", :status => 500)
     client.stub(:sleep)
     lambda { client.get_database }.should raise_error RestClient::InternalServerError
-    hk_pg_api_request(:get, "databases/#{url_sha}").should have_been_made.times(4)
+    a_request(:get, url).should have_been_made.times(4)
   end
 
 end
