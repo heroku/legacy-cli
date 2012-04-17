67301e7d35f55e600e88766989969bd3aa80f487
Brandur <brandur@mutelight.org>
2012-04-16 17:10:41 -0700

    Working certs commands

---
+++ lib/heroku/client/ssl_endpoint.rb
@@ -0,0 +1,26 @@
+class Heroku::Client
+  def ssl_endpoint_add(app, pem, key)
+    raise RestClient::RequestTimeout
+    json_decode(post("v3/apps/#{app}/ssl_endpoints", :accept => :json, :pem => pem, :key => key).to_s)
+  end
+
+  def ssl_endpoint_info(app, cname)
+    json_decode(get("v3/apps/#{app}/ssl_endpoints/#{escape(cname)}", :accept => :json).to_s)
+  end
+
+  def ssl_endpoint_list(app)
+    json_decode(get("v3/apps/#{app}/ssl_endpoints", :accept => :json).to_s)
+  end
+
+  def ssl_endpoint_remove(app, cname)
+    json_decode(delete("v3/apps/#{app}/ssl_endpoints/#{escape(cname)}", :accept => :json).to_s)
+  end
+
+  def ssl_endpoint_rollback(app, cname)
+    json_decode(post("v3/apps/#{app}/ssl_endpoints/#{escape(cname)}/rollback", :accept => :json).to_s)
+  end
+
+  def ssl_endpoint_update(app, cname, pem, key)
+    json_decode(put("v3/apps/#{app}/ssl_endpoints/#{escape(cname)}", :accept => :json, :pem => pem, :key => key).to_s)
+  end
+end
