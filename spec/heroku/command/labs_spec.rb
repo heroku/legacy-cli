e4dffb33933bf111ecaf867d8494b2761135d7d7
geemus <geemus@gmail.com>
2012-04-17 13:12:35 -0500

    cleanup/standardize labs tests

--- spec/heroku/command/labs_spec.rb
+++ spec/heroku/command/labs_spec.rb
@@ -7,53 +7,84 @@ module Heroku::Command
       @labs = prepare_command(Labs)
       @labs.heroku.stub!(:info).and_return({})
     end
-    
+
     it "lists no features if developer is not enrolled" do
-      Heroku::Auth.stub!("user").and_return("user")
-      @labs.heroku.should_receive(:list_features).with('myapp').and_return([])
-      @labs.index
+      stub_core.list_features("myapp").returns([])
+      stderr, stdout = execute("labs")
+      stderr.should == ""
+      stdout.should == <<-STDOUT
+=== App Features (myapp)
+
+=== User Features (user)
+STDOUT
     end
 
     it "lists features if developer is enrolled" do
-      Heroku::Auth.stub!("user").and_return("user")
-      @labs.heroku.should_receive(:list_features).with('myapp').and_return([])
-      @labs.index
+      stub_core.list_features("myapp").returns([])
+      stderr, stdout = execute("labs")
+      stderr.should == ""
+      stdout.should == <<-STDOUT
+=== App Features (myapp)
+
+=== User Features (user)
+STDOUT
     end
-    
+
     it "displays details of a feature" do
-      @labs.stub!(:args).and_return(['example'])
-      @labs.heroku.should_receive(:get_feature).with('myapp', 'example').and_return({'docs' => 'http://devcenter.heroku.com/labs-example', 'name' => 'example', 'summary' => 'example feature'})
-      @labs.info
+      stub_core.get_feature('myapp', 'example').returns({'docs' => 'http://devcenter.heroku.com/labs-example', 'name' => 'example', 'summary' => 'example feature'})
+      stderr, stdout = execute("labs:info example")
+      stderr.should == ""
+      stdout.should == <<-STDOUT
+=== example
+Summary: example feature
+Docs:    http://devcenter.heroku.com/labs-example
+STDOUT
     end
-    
+
     it "shows usage if no feature name is specified for info" do
-      @labs.stub!(:args).and_return([])
-      STDERR.should_receive(:puts).with(" !    Usage: heroku labs:info FEATURE")
-      lambda { @labs.info }.should raise_error(SystemExit)
+      stderr, stdout = execute("labs:info")
+      stderr.should == <<-STDERR
+ !    Usage: heroku labs:info FEATURE
+STDERR
+      # FIXME: sometimes stdout is "failed"
+      #stdout.should == "failed"
     end
-    
+
     it "enables a feature" do
-      @labs.stub!(:args).and_return(['example'])
-      @labs.heroku.should_receive(:enable_feature).with('myapp', 'example')
-      @labs.enable
+      stub_core.enable_feature('myapp', 'example')
+      stderr, stdout = execute("labs:enable example")
+      stderr.should == ""
+      stdout.should == <<-STDOUT
+----> Enabling example for myapp... done
+WARNING: This feature is experimental and may change or be removed without notice.
+STDOUT
     end
-    
+
     it "shows usage if no feature name is specified for enable" do
-      @labs.stub!(:args).and_return([])
-      STDERR.should_receive(:puts).with(" !    Usage: heroku labs:enable FEATURE")
-      lambda { @labs.enable }.should raise_error(SystemExit)
+      stderr, stdout = execute("labs:enable")
+      stderr.should == <<-STDERR
+ !    Usage: heroku labs:enable FEATURE
+STDERR
+      # FIXME: sometimes stdout is "failed"
+      #stdout.should == "failed"
     end
-    
+
     it "disables a feature" do
-      @labs.stub!(:args).and_return(['example'])
-      @labs.heroku.should_receive(:disable_feature).with('myapp', 'example')
-      @labs.disable
+      stub_core.disable_feature('myapp', 'example')
+      stderr, stdout = execute("labs:disable example")
+      stderr.should == ""
+      stdout.should == <<-STDOUT
+----> Disabling example for myapp... done
+STDOUT
     end
-    
+
     it "shows usage if no feature name is specified for disable" do
-      @labs.stub!(:args).and_return([])
-      STDERR.should_receive(:puts).with(" !    Usage: heroku labs:disable FEATURE")
-      lambda { @labs.disable }.should raise_error(SystemExit)
+      stderr, stdout = execute("labs:disable")
+      stderr.should == <<-STDERR
+ !    Usage: heroku labs:disable FEATURE
+STDERR
+      # FIXME: sometimes stdout is "failed"
+      #stdout.should == "failed"
     end
   end
 end
