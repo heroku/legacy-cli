feb459bcb6bc806df6ab4131e5bf2aefcdc44185
geemus <geemus@gmail.com>
2012-04-19 09:06:51 +0100

    cleanup/standardize version specs

--- spec/heroku/command/version_spec.rb
+++ spec/heroku/command/version_spec.rb
@@ -3,14 +3,14 @@ require "heroku/command/version"
 
 module Heroku::Command
   describe Version do
-    before(:each) do
-      @cli = prepare_command(Version)
-      @cli.stub(:options).and_return(:app => "myapp")
+
+    it "shows version info" do
+      stderr, stdout = execute("version")
+      stderr.should == ""
+      stdout.should == <<-STDOUT
+#{Heroku::VERSION}
+STDOUT
     end
 
-    it "shows app info using the --app syntax" do
-      @cli.should_receive(:display).with(Heroku::VERSION)
-      @cli.index
-    end
   end
 end
