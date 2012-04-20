ec59392f0af31eaa6c02928e4453665e564f2d3b
Brandur <brandur@mutelight.org>
2012-04-19 18:09:47 -0700

    Use new SSL endpoints API namespace
    The SSL endpoints API being in the v3/ URL namespace never really made
    sense, so this change moves the Gem over to a more consistent SSL
    endpoints API before certs goes GA.
    
    The old `v3/apps/:app_id/ssl_endpoints` namespace will continue to
    function normally for the time being for all clients who used certs
    throughout the beta period.

--- spec/heroku/client/ssl_endpoint_spec.rb
+++ spec/heroku/client/ssl_endpoint_spec.rb
@@ -7,20 +7,20 @@ describe Heroku::Client, "ssl endpoints" do
   end
 
   it "adds an ssl endpoint" do
-    stub_request(:post, "https://api.heroku.com/v3/apps/myapp/ssl_endpoints").
+    stub_request(:post, "https://api.heroku.com/apps/myapp/ssl-endpoints").
       with(:body => { :accept => "json", :pem => "pem content", :key => "key content" }).
       to_return(:body => %{ {"cname": "tokyo-1050" } })
     @client.ssl_endpoint_add("myapp", "pem content", "key content").should == { "cname" => "tokyo-1050" }
   end
 
   it "gets info on an ssl endpoint" do
-    stub_request(:get, "https://api.heroku.com/v3/apps/myapp/ssl_endpoints/tokyo-1050").
+    stub_request(:get, "https://api.heroku.com/apps/myapp/ssl-endpoints/tokyo-1050").
       to_return(:body => %{ {"cname": "tokyo-1050" } })
     @client.ssl_endpoint_info("myapp", "tokyo-1050").should == { "cname" => "tokyo-1050" }
   end
 
   it "lists ssl endpoints for an app" do
-    stub_request(:get, "https://api.heroku.com/v3/apps/myapp/ssl_endpoints").
+    stub_request(:get, "https://api.heroku.com/apps/myapp/ssl-endpoints").
       to_return(:body => %{ [{"cname": "tokyo-1050" }, {"cname": "tokyo-1051" }] })
     @client.ssl_endpoint_list("myapp").should == [
       { "cname" => "tokyo-1050" },
@@ -29,18 +29,18 @@ describe Heroku::Client, "ssl endpoints" do
   end
 
   it "removes an ssl endpoint" do
-    stub_request(:delete, "https://api.heroku.com/v3/apps/myapp/ssl_endpoints/tokyo-1050")
+    stub_request(:delete, "https://api.heroku.com/apps/myapp/ssl-endpoints/tokyo-1050")
     @client.ssl_endpoint_remove("myapp", "tokyo-1050")
   end
 
   it "rolls back an ssl endpoint" do
-    stub_request(:post, "https://api.heroku.com/v3/apps/myapp/ssl_endpoints/tokyo-1050/rollback").
+    stub_request(:post, "https://api.heroku.com/apps/myapp/ssl-endpoints/tokyo-1050/rollback").
       to_return(:body => %{ {"cname": "tokyo-1050" } })
     @client.ssl_endpoint_rollback("myapp", "tokyo-1050").should == { "cname" => "tokyo-1050" }
   end
 
   it "updates an ssl endpoint" do
-    stub_request(:put, "https://api.heroku.com/v3/apps/myapp/ssl_endpoints/tokyo-1050").
+    stub_request(:put, "https://api.heroku.com/apps/myapp/ssl-endpoints/tokyo-1050").
       with(:body => { :accept => "json", :pem => "pem content", :key => "key content" }).
       to_return(:body => %{ {"cname": "tokyo-1050" } })
     @client.ssl_endpoint_update("myapp", "tokyo-1050", "pem content", "key content").should == { "cname" => "tokyo-1050" }
