508c311c5478a6e4be5cbfe55867c0a46ce7e25a
geemus <geemus@gmail.com>
2012-05-16 16:58:41 -0700

    ideal update commands

--- lib/heroku/command/update.rb
+++ lib/heroku/command/update.rb
@@ -1,35 +1,44 @@
 require "heroku/command/base"
 require "heroku/updater"
 
-module Heroku::Command
+# update the heroku client
+#
+class Heroku::Command::Update < Heroku::Command::Base
 
+  # update
+  #
   # update the heroku client
-  class Update < Base
-
-    # update
-    #
-    # update the heroku client
-    #
-    def index
-      if message = Heroku::Updater.disable
-        error message
-      end
-
-      action("Updating from #{Heroku::VERSION}") do
-        Heroku::Updater.update
-        /VERSION = "([^"]+)"/ =~ File.read(File.join(Heroku::Updater.updated_client_path, "lib/heroku/version.rb"))
-        status("updated to #{$1}")
-      end
+  #
+  # Example:
+  #
+  # $ heroku update
+  # Updating from v1.2.3... done, updated to v2.3.4
+  #
+  def index
+    if message = Heroku::Updater.disable
+      error message
     end
 
-    # update:beta
-    #
-    # update to the latest beta client
-    #
-    def beta
-      action("Updating to latest beta client") do
-        Heroku::Updater.update(true)
-      end
+    action("Updating from #{Heroku::VERSION}") do
+      Heroku::Updater.update
+      /VERSION = "([^"]+)"/ =~ File.read(File.join(Heroku::Updater.updated_client_path, "lib/heroku/version.rb"))
+      status("updated to #{$1}")
     end
   end
+
+  # update:beta
+  #
+  # update to the latest beta client
+  #
+  # $ heroku update
+  # Updating from v1.2.3... done, updated to v2.3.4.pre
+  #
+  def beta
+    action("Updating from #{Heroku::VERSION}") do
+      Heroku::Updater.update(true)
+      /VERSION = "([^"]+)"/ =~ File.read(File.join(Heroku::Updater.updated_client_path, "lib/heroku/version.rb"))
+      status("updated to #{$1}")
+    end
+  end
+
 end
