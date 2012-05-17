4df7d2faada09cd0c2f667cfe675e6265be59f9f
geemus <geemus@gmail.com>
2012-05-16 17:03:49 -0700

    ideal account

--- lib/heroku/command/account.rb
+++ lib/heroku/command/account.rb
@@ -1,23 +1,28 @@
 require "heroku/command/base"
 
-module Heroku::Command
+# manage heroku account options
+#
+class Heroku::Command::Account < Heroku::Command::Base
 
-  # manage heroku account options
+  # account:confirm_billing
   #
-  class Account < Base
-
-    # account:confirm_billing
-    #
-    # Confirm that your account can be billed at the end of the month
-    #
-    def confirm_billing
-      display(" This action will cause your account to be billed at the end of the month")
-      display(" For more information, see http://docs.heroku.com/billing")
-      display(" Are you sure you want to do this? (y/n) ", false)
-      if ask.downcase == 'y'
-        heroku.confirm_billing
-        return true
-      end
+  # Confirm that your account can be billed at the end of the month
+  #
+  #Example:
+  #
+  # $ heroku account:confirm_billing
+  #  This action will cause your account to be billed at the end of the month
+  #  For more information, see http://docs.heroku.com/billing
+  #  Are you sure you want to do this? (y/n) y
+  #
+  def confirm_billing
+    display(" This action will cause your account to be billed at the end of the month")
+    display(" For more information, see http://docs.heroku.com/billing")
+    display(" Are you sure you want to do this? (y/n) ", false)
+    if ask.downcase == 'y'
+      heroku.confirm_billing
+      return true
     end
   end
+
 end
