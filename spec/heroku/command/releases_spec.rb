22cf5b7841f255fbedac005fd66c4d00f70dcbea
David Dollar <ddollar@gmail.com>
2012-05-30 11:37:11 -0400

    add spec to cover release description truncation

--- spec/heroku/command/releases_spec.rb
+++ spec/heroku/command/releases_spec.rb
@@ -12,18 +12,20 @@ describe Heroku::Command::Releases do
     before(:each) do
       api.post_app("name" => "myapp", "stack" => "cedar")
       @now = Time.now
-      Time.should_receive(:now).exactly(9).times.and_return(@now)
+      Time.should_receive(:now).exactly(11).times.and_return(@now)
       times = [
         @now - 60 * 60 * 24 * 2,
         @now - 60 * 60 * 12,
         @now - 60 * 30,
+        @now - 30,
         @now - 30
       ].map {|time| time.strftime("%G/%m/%d %H:%M:%S %z")}
-      Heroku::API::Mock.should_receive(:timestamp).exactly(4).times.and_return(*times)
+      Heroku::API::Mock.should_receive(:timestamp).exactly(5).times.and_return(*times)
       api.put_config_vars("myapp", { 'FOO_BAR'  => 'BAZ' })
       api.put_config_vars("myapp", { 'BAR_BAZ'  => 'QUX' })
       api.put_config_vars("myapp", { 'BAZ_QUX'  => 'QUUX' })
       api.put_config_vars("myapp", { 'QUX_QUUX' => 'XYZZY' })
+      api.put_config_vars("myapp", { 'SUPER_LONG_CONFIG_VAR_TO_GET_PAST_THE_TRUNCATION_LIMIT' => 'VALUE' })
     end
 
     after(:each) do
@@ -35,10 +37,11 @@ describe Heroku::Command::Releases do
       @stderr.should == ""
       @stdout.should == <<-STDOUT
 === myapp Releases
-v4   Config add QUX_QUUX   email@example.com   30s ago
-v3   Config add BAZ_QUX    email@example.com   30m ago
-v2   Config add BAR_BAZ    email@example.com   12h ago
-v1   Config add FOO_BAR    email@example.com   #{(@now - 60 * 60 * 24 * 2).strftime("%G/%m/%d %H:%M:%S")}
+v5   Config add SUPER_LONG_CONFIG_VAR_TO_GE..   email@example.com   30s ago
+v4   Config add QUX_QUUX                        email@example.com   30s ago
+v3   Config add BAZ_QUX                         email@example.com   30m ago
+v2   Config add BAR_BAZ                         email@example.com   12h ago
+v1   Config add FOO_BAR                         email@example.com   #{(@now - 60 * 60 * 24 * 2).strftime("%G/%m/%d %H:%M:%S")}
 
 STDOUT
     end
