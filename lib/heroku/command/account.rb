0ec8b515bb5d07541c8a6987a5548ce3399b03da
geemus <geemus@gmail.com>
2012-06-05 07:57:24 -0500

    fix billing info link in confirm_billing

--- lib/heroku/command/account.rb
+++ lib/heroku/command/account.rb
@@ -19,7 +19,7 @@ class Heroku::Command::Account < Heroku::Command::Base
     validate_arguments!
 
     display(" This action will cause your account to be billed at the end of the month")
-    display(" For more information, see http://docs.heroku.com/billing")
+    display(" For more information, see https://devcenter.heroku.com/articles/usage-and-billing")
     display(" Are you sure you want to do this? (y/n) ", false)
     if ask.downcase == 'y'
       heroku.confirm_billing
