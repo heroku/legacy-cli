170256cfac3b96d413823ca1b1662f0cfe7b56cc
geemus <geemus@gmail.com>
2012-06-12 10:16:01 -0500

    move Interrupt rescue to CLI start so toolbelts can take advantage

--- lib/heroku/cli.rb
+++ lib/heroku/cli.rb
@@ -4,9 +4,14 @@ require "heroku/command"
 class Heroku::CLI
 
   def self.start(*args)
-    command = args.shift.strip rescue "help"
-    Heroku::Command.load
-    Heroku::Command.run(command, args)
+    begin
+      command = args.shift.strip rescue "help"
+      Heroku::Command.load
+      Heroku::Command.run(command, args)
+    rescue Interrupt
+      `stty icanon echo`
+      puts("\n !    Command cancelled.")
+    end
   end
 
 end
