a10d1997d5e084bf30852390994dee48d189a373
geemus <geemus@gmail.com>
2012-06-12 17:24:25 -0500

    add more info to user-agent and give toolbelt it's own

--- lib/heroku.rb
+++ lib/heroku.rb
@@ -2,6 +2,6 @@ require "heroku/client"
 
 module Heroku
 
-  USER_AGENT = "heroku-gem/#{Heroku::VERSION}"
+  USER_AGENT = "heroku-gem/#{Heroku::VERSION} (#{RUBY_PLATFORM}) ruby/#{RUBY_VERSION}"
 
 end
