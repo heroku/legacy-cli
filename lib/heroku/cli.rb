b175a043268ada95d0cdc93b1755d21661b2c578
geemus <geemus@gmail.com>
2012-06-14 10:21:14 -0500

    first pass at nicer error formatting to facilitate reporting

--- lib/heroku/cli.rb
+++ lib/heroku/cli.rb
@@ -11,6 +11,27 @@ class Heroku::CLI
     rescue Interrupt
       `stty icanon echo`
       puts("\n !    Command cancelled.")
+    rescue => error
+      puts(' !    Error encountered.')
+      puts(" !    You can search for help at: https://help.heroku.com")
+      puts(" !    Or report this as a bug at: https://github.com/heroku/heroku/issues/new")
+      puts
+      puts("    Error:     #{error.message} (#{error.class})")
+      puts("    Backtrace: #{error.backtrace.first}")
+      error.backtrace[1..-1].each do |line|
+        puts("               #{line}")
+      end
+      puts
+      command = ARGV.map do |arg|
+        if arg.include?(' ')
+          arg = %{"#{arg}"}
+        else
+          arg
+        end
+      end.join(' ')
+      puts("    Command:   heroku #{command}")
+      puts("    Version:   #{Heroku::USER_AGENT}")
+      puts
     end
   end
 
