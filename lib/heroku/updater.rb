7148227adbaa40a9c1d0a1630804c9f69e42dab7
geemus <geemus@gmail.com>
2012-06-22 09:40:07 -0500

    reload updater in inject_libpath closes #396

--- lib/heroku/updater.rb
+++ lib/heroku/updater.rb
@@ -110,6 +110,7 @@ module Heroku
         vendored_gems.each do |vendored_gem|
           $:.unshift File.join(vendored_gem, "lib")
         end
+        load('heroku/updater.rb') # reload updated updater
       end
     end
 
