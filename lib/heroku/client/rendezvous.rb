8099f3dfed25e80f2c26e6a75653a30d02d8b912
David Dollar <ddollar@gmail.com>
2012-07-19 12:28:34 -0400

    use heroku/auth's verification in rendezvous

--- lib/heroku/client/rendezvous.rb
+++ lib/heroku/client/rendezvous.rb
@@ -1,6 +1,7 @@
 require "timeout"
 require "socket"
 require "uri"
+require "heroku/auth"
 require "heroku/client"
 require "heroku/helpers"
 
@@ -29,10 +30,12 @@ class Heroku::Client::Rendezvous
 
     ssl_socket = Timeout.timeout(connect_timeout) do
       ssl_context = OpenSSL::SSL::SSLContext.new
-      if ((host =~ /heroku\.com$/) && !(ENV["HEROKU_SSL_VERIFY"] == "disable"))
-        ssl_context.ca_file = File.expand_path("../../../../data/cacert.pem", __FILE__)
+
+      if Heroku::Auth.verify_host?(host)
+        ssl_context.ca_file     = File.expand_path("../../../../data/cacert.pem", __FILE__)
         ssl_context.verify_mode = OpenSSL::SSL::VERIFY_PEER
       end
+
       tcp_socket = TCPSocket.open(host, port)
       ssl_socket = OpenSSL::SSL::SSLSocket.new(tcp_socket, ssl_context)
       ssl_socket.connect
