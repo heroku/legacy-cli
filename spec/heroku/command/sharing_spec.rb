87fcaa4a184b3737677b2955945dad2c3808fcb8
Dominic Dagradi <dominic@heroku.com>
2013-12-16 13:44:34 -0800

    Update sharing spec copy

--- spec/heroku/command/sharing_spec.rb
+++ spec/heroku/command/sharing_spec.rb
@@ -33,7 +33,7 @@ STDOUT
       stderr, stdout = execute("sharing:add collaborator@example.com")
       stderr.should == ""
       stdout.should == <<-STDOUT
-Adding collaborator@example.com to example collaborators... done
+Adding collaborator@example.com to example as collaborator... done
 STDOUT
     end
 
