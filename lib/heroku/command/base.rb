9e445fb87c2a55677cc55bbfe8804bfff787a032
Dominic Dagradi <dominic@heroku.com>
2013-12-16 14:09:26 -0800

    Support manual HEROKU_MANAGER_URL override regardless of cloud

--- lib/heroku/command/base.rb
+++ lib/heroku/command/base.rb
@@ -255,7 +255,7 @@ protected
   end
 
   def skip_org?
-    return false unless ENV['HEROKU_CLOUD']
+    return false if ENV['HEROKU_CLOUD'].nil? || ENV['HEROKU_MANAGER_URL']
 
     !%w{default production prod}.include? ENV['HEROKU_CLOUD']
   end
