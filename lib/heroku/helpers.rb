dec4b3ef6ce66382c85da4260df3230192893a48
Raul Barroso <ra.barroso@gmail.com>
2014-04-24 12:38:09 -0700

    s/organzation/organization

--- lib/heroku/helpers.rb
+++ lib/heroku/helpers.rb
@@ -221,7 +221,7 @@ module Heroku
     ## DISPLAY HELPERS
 
     def action(message, options={})
-      message = "#{message} in organzation #{org}" if options[:org]
+      message = "#{message} in organization #{org}" if options[:org]
       display("#{message}... ", false)
       Heroku::Helpers.error_with_failure = true
       ret = yield
