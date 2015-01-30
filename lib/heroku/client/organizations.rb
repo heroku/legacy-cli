22003669ee9f59681b61887775925ecbe3e21059
dickeyxxx <jeff@dickeyxxx.com>
2015-01-30 09:37:20 -0800

    remove default org functionality in favor of HEROKU_ORGANIZATION

--- lib/heroku/client/organizations.rb
+++ lib/heroku/client/organizations.rb
@@ -84,24 +84,6 @@ class Heroku::Client::Organizations
       end
     end
 
-    def remove_default_org
-      api.request(
-        :expects => 204,
-        :method => :delete,
-        :path => "/v1/user/default-organization"
-      )
-    end
-
-    def set_default_org(org)
-      api.request(
-        :expects => 200,
-        :method => :post,
-        :path => "/v1/user/default-organization",
-        :body => Heroku::Helpers.json_encode( { "default_organization" => org } ),
-        :headers => {"Content-Type" => "application/json"}
-      )
-    end
-
     # Apps
     #################################
     def get_apps(org)
--- spec/heroku/command/orgs_spec.rb
+++ spec/heroku/command/orgs_spec.rb
@@ -42,7 +42,7 @@ STDOUT
       it "labels a user's default organization" do
         Excon.stub({ :method => :get, :path   => '/v1/user/info' },
           {
-            :body   => MultiJson.dump({"organizations" => [{"organization_name" => "test-org", "role" => "collaborator"}, {"organization_name" => "test-org2", "role" => "admin"}], "user" => {"default_organization" => "test-org2"}}),
+            :body   => MultiJson.dump({"organizations" => [{"organization_name" => "test-org", "role" => "collaborator"}, {"organization_name" => "test-org2", "role" => "admin"}]}),
             :status => 200
           }
         )
@@ -51,66 +51,9 @@ STDOUT
         expect(stderr).to eq("")
         expect(stdout).to eq <<-STDOUT
 test-org   collaborator
-test-org2  admin, default
+test-org2  admin
 
 STDOUT
-      end
-    end
-
-    context(:default) do
-      context "when a target org is specified" do
-        it "sets the default org to the target" do
-          expect(org_api).to receive(:set_default_org).with("test-org").once
-          stderr, stdout = execute("orgs:default test-org")
-          expect(stderr).to eq("")
-          expect(stdout).to eq <<-STDOUT
-Setting test-org as the default organization... done
-STDOUT
-        end
-
-        it "removes the default org when the org name is 'personal'" do
-          expect(org_api).to receive(:remove_default_org).once
-          stderr, stdout = execute("orgs:default personal")
-          expect(stderr).to eq("")
-          expect(stdout).to eq <<-STDOUT
-Setting personal account as default... done
-STDOUT
-        end
-
-        it "removes the defautl org when the personal flag is passed" do
-          expect(org_api).to receive(:remove_default_org).once
-          stderr, stdout = execute("orgs:default --personal")
-          expect(stderr).to eq("")
-          expect(stdout).to eq <<-STDOUT
-Setting personal account as default... done
-STDOUT
-        end
-
-      end
-
-      context "when no target is specified" do
-        it "displays the default organization when present" do
-          Excon.stub({ :method => :get, :path   => '/v1/user/info' },
-            {
-              :body   => MultiJson.dump({"user" => {"default_organization" => "test-org"}}),
-              :status => 200
-            }
-          )
-
-          stderr, stdout = execute("orgs:default")
-          expect(stderr).to eq("")
-          expect(stdout).to eq <<-STDOUT
-test-org is the default organization.
-STDOUT
-        end
-
-        it "displays personal account as default when no org present" do
-          stderr, stdout = execute("orgs:default")
-          expect(stderr).to eq("")
-          expect(stdout).to eq <<-STDOUT
-Personal account is default.
-STDOUT
-        end
       end
     end
 
@@ -121,24 +64,19 @@ STDOUT
       end
 
       it "opens the org specified in an argument" do
-        stderr, stdout = execute("orgs:open --org test-org")
+        _, stdout = execute("orgs:open --org test-org")
         expect(stdout).to eq <<-STDOUT
 Opening web interface for test-org... done
 STDOUT
       end
 
-      it "opens the default org" do
-        Excon.stub({ :method => :get, :path   => '/v1/user/info' },
-          {
-            :body   => MultiJson.dump({"organizations" => [{"organization_name" => "test-org"}], "user" => {"default_organization" => "test-org"}}),
-            :status => 200
-          }
-        )
-
-        stderr, stdout = execute("orgs:open")
+      it "opens the org specified in HEROKU_ORGANIZATION" do
+        ENV['HEROKU_ORGANIZATION'] = 'test-org'
+        _, stdout = execute("orgs:open")
         expect(stdout).to eq <<-STDOUT
 Opening web interface for test-org... done
 STDOUT
+        ENV['HEROKU_ORGANIZATION'] = nil
       end
     end
   end
