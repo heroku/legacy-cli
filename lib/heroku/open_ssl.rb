572c401a258b56ed24b2482bf4bf5cb9478cb338
dickeyxxx <jeff@dickeyxxx.com>
2015-03-19 10:21:50 -0700

    cleaned up text and removed pending spec from open_ssl specs

--- lib/heroku/open_ssl.rb
+++ lib/heroku/open_ssl.rb
@@ -10,21 +10,21 @@ module Heroku
         system(openssl, *args)
       end
     end
-    
+
     def self.openssl=(val)
       @checked = false
       ENV["OPENSSL"] = val
     end
-    
+
     class CertificateRequest
       attr_accessor :domain, :subject, :key_size, :self_signed
-      
+
       def initialize()
         @key_size = 2048
         @self_signed = false
         super
       end
-      
+
       def generate
         if self_signed
           generate_self_signed_certificate
@@ -32,46 +32,46 @@ module Heroku
           generate_csr
         end
       end
-      
+
       class Result
         attr_accessor :request, :key_file, :csr_file, :crt_file
-        
+
         def initialize(request, key_file, csr_file, crt_file)
           @request = request.dup
           @key_file, @csr_file, @crt_file = key_file, csr_file, crt_file
         end
       end
-      
-    private
+
+      private
       def generate_csr
         keyfile = "#{domain}.key"
         csrfile = "#{domain}.csr"
-        
+
         openssl_req_new(keyfile, csrfile) or raise GenericError, "Key and CSR generation failed: #{$?}"
-        
+
         return Result.new(self, keyfile, csrfile, nil)
       end
-    
+
       def generate_self_signed_certificate
         keyfile = "#{domain}.key"
         crtfile = "#{domain}.crt"
-        
+
         openssl_req_new(keyfile, crtfile, "-x509") or raise GenericError, "Key and self-signed certificate generation failed: #{$?}"
-        
+
         return Result.new(self, keyfile, nil, crtfile)
       end
-      
+
       def openssl_req_new(keyfile, outfile, *args)
         Heroku::OpenSSL.ensure_openssl_installed!
         Heroku::OpenSSL.openssl("req", "-new", "-newkey", "rsa:#{key_size}", "-nodes", "-keyout", keyfile, "-out", outfile, "-subj", subject, *args)
       end
     end
-    
+
     class GenericError < StandardError; end
 
     class NotInstalledError < GenericError
       include Heroku::Helpers
-  
+
       def installation_hint
         if running_on_a_mac?
           "With Homebrew <http://brew.sh> installed, run the following command:\n$ brew install openssl"
@@ -83,7 +83,7 @@ module Heroku
         end
       end
     end
-    
+
     def self.ensure_openssl_installed!
       return if @checked
       openssl("version") or raise NotInstalledError
