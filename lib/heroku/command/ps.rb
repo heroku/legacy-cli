b698d9216a5772e47e901c918727a336c8d75222
Will Leinweber <will@bitfission.com>
2015-04-29 17:04:14 -0700

    import the dyno-types plugin

--- lib/heroku/command/ps.rb
+++ lib/heroku/command/ps.rb
@@ -1,12 +1,25 @@
+require "json"
 require "heroku/command/base"
 
 # manage dynos (dynos, workers)
 #
 class Heroku::Command::Ps < Heroku::Command::Base
-  PRICES = {
-    "P"  => 0.8,
-    "PX" => 0.8,
-  }
+  PROCESS_TIERS = JSON.parse <<EOF
+[
+  { "tier": "free",        "max_scale": 1,   "max_processes": 2,    "cost": { "Free": 0 } },
+  { "tier": "hobby",       "max_scale": 1,   "max_processes": null, "cost": { "Hobby": 700 } },
+  { "tier": "production",  "max_scale": 100, "max_processes": null, "cost": { "Standard-1X": 2500, "Standard-2X": 5000, "Performance": 50000 } },
+  { "tier": "traditional", "max_scale": 100, "max_processes": null, "cost": {          "1X": 3600,          "2X": 7200,          "PX": 57600 } }
+]
+EOF
+
+  costs = PROCESS_TIERS.collect do |tier|
+    tier["cost"].collect do |name, cents_per_month|
+      [name, (cents_per_month / 100)]
+    end
+  end
+  COSTS = Hash[*costs.flatten]
+
 
   # ps:dynos [QTY]
   #
@@ -262,23 +275,132 @@ class Heroku::Command::Ps < Heroku::Command::Base
 
   alias_command "stop", "ps:stop"
 
-  # ps:resize DYNO1=1X|2X|PX [DYNO2=1X|2X|PX ...]
+  # ps:type [TYPE | DYNO=TYPE [DYNO=TYPE ...]]
   #
-  # resize dynos to the given size
+  # manage dyno types
   #
-  # Example:
+  # called with no arguments shows the current dyno type
   #
-  # $ heroku ps:resize web=PX worker=2X
-  # Resizing and restarting the specified dynos... done
-  # web dynos now PX ($0.80/dyno-hour)
-  # worker dynos now 2X ($0.10/dyno-hour)
+  # called with one argument sets the type
+  # where type is one of traditional|free|hobby|standard-1x|standard-2x|performance
   #
-  def resize
+  # called with 1..n DYNO=TYPE arguments sets the type per dyno
+  # this is only available when the app is on production and performance
+  #
+  def type
+    if args.any?{|arg| arg =~ /=/}
+      _original_resize
+      return
+    end
+
+    app
+    process_tier = shift_argument
+    process_tier.downcase! if process_tier
+    validate_arguments!
+
+    if %w[standard-1x standard-2x performance].include?(process_tier)
+      special_case_change_tier_and_resize(process_tier)
+      return
+    end
+
+    # get or update app.process_tier
+    app_resp = process_tier.nil? ? edge_app_info : change_dyno_type(process_tier)
+
+    # get, calculate and display app process type costs
+    formation_resp = edge_app_formation
+
+    display_dyno_type_and_costs(app_resp, formation_resp)
+  end
+
+  alias_method :resize, :type
+  alias_command "resize", "ps:type"
+
+  private
+
+  def change_dyno_type(process_tier)
+    print "Changing dyno type... "
+
+    app_resp = patch_tier(process_tier)
+
+    if app_resp.status != 200
+      puts "failed"
+      error app_resp.body["message"] + " Please use `heroku ps:scale` to change process size and scale."
+    end
+
+    puts "done."
+
+    return app_resp
+  end
+
+  def patch_tier(process_tier)
+    api.request(
+      :method  => :patch,
+      :path    => "/apps/#{app}",
+      :body    => json_encode("process_tier" => process_tier),
+      :headers => {
+        "Accept"       => "application/vnd.heroku+json; version=edge",
+        "Content-Type" => "application/json"
+      }
+    )
+  end
+
+  def display_dyno_type_and_costs(app_resp, formation_resp)
+    tier_info = PROCESS_TIERS.detect { |t| t["tier"] == app_resp.body["process_tier"] }
+
+    formation = formation_resp.body.reject {|ps| ps['quantity'] < 1}
+
+    annotated = formation.sort_by{|d| d['type']}.map do |dyno|
+      cost = tier_info["cost"][dyno["size"]] * dyno["quantity"] / 100
+      {
+        'dyno'    => dyno['type'],
+        'type'    => dyno['size'].rjust(4),
+        'qty'     => dyno['quantity'].to_s.rjust(3),
+        'cost/mo' => cost.to_s.rjust(7)
+      }
+    end
+
+    # in case of an app not yet released
+    annotated = [tier_info] if annotated.empty?
+
+    display_table(annotated, annotated.first.keys, annotated.first.keys)
+  end
+
+  def edge_app_info
+    api.request(
+      :expects => 200,
+      :method  => :get,
+      :path    => "/apps/#{app}",
+      :headers => {
+        "Accept"       => "application/vnd.heroku+json; version=edge",
+        "Content-Type" => "application/json"
+      }
+    )
+  end
+
+  def edge_app_formation
+    api.request(
+      :expects => 200,
+      :method  => :get,
+      :path    => "/apps/#{app}/formation",
+      :headers => {
+        "Accept"       => "application/vnd.heroku+json; version=3",
+        "Content-Type" => "application/json"
+      }
+    )
+  end
+
+  def special_case_change_tier_and_resize(type)
+    patch_tier("production")
+    override_args = edge_app_formation.body.map { |ps| "#{ps['type']}=#{type}" }
+    _original_resize(override_args)
+  end
+
+  def _original_resize(override_args=nil)
     app
     change_map = {}
 
-    changes = args.map do |arg|
-      if arg =~ /^([a-zA-Z0-9_]+)=(\w+)$/
+    changes = (override_args || args).map do |arg|
+      if arg =~ /^([a-zA-Z0-9_]+)=([\w-]+)$/
         change_map[$1] = $2
         { "process" => $1, "size" => $2 }
       end
@@ -286,8 +408,8 @@ class Heroku::Command::Ps < Heroku::Command::Base
 
     if changes.empty?
       message = [
-          "Usage: heroku ps:resize DYNO1=1X|2X|PX [DYNO2=1X|2X|PX ...]",
-          "Must specify DYNO and SIZE to resize."
+          "Usage: heroku dyno:type DYNO1=1X|2X|PX [DYNO2=1X|2X|PX ...]",
+          "Must specify DYNO and TYPE to resize."
       ]
       error(message.join("\n"))
     end
@@ -308,14 +430,12 @@ class Heroku::Command::Ps < Heroku::Command::Base
 
     resp.body.select {|p| change_map.key?(p['type']) }.each do |p|
       size = p["size"]
-      price = if size.to_i > 0
-                sprintf("%.2f", 0.05 * size.to_i)
-              else
-                sprintf("%.2f", PRICES[size])
-              end
-      display "#{p["type"]} dynos now #{size} ($#{price}/dyno-hour)"
+      display "#{p["type"]} dynos now #{size} ($#{COSTS[size]}/month)"
     end
   end
-
-  alias_command "resize", "ps:resize"
 end
+
+%w[type restart scale stop].each do |cmd|
+  Heroku::Command::Base.alias_command "dyno:#{cmd}", "ps:#{cmd}"
+end
+
