90c157fea9b93394454b6ad8cff86e03cf59c2f9
Kara Louie <kara@heroku.com>
2015-05-08 16:05:16 -0700

    Bug fixes for users with partial privileges on apps:info

--- lib/heroku/command/apps.rb
+++ lib/heroku/command/apps.rb
@@ -103,7 +103,11 @@ class Heroku::Command::Apps < Heroku::Command::Base
       styled_header(app_data["name"])
     end
 
-    addons_data = api.get_addons(app).body.map {|addon| addon['name']}.sort
+    begin
+      addons_data = api.get_addons(app).body.map {|addon| addon['name']}.sort
+    rescue
+      addons_data = {}
+    end
     collaborators_data = api.get_collaborators(app).body.map {|collaborator| collaborator["email"]}.sort
     collaborators_data.reject! {|email| email == app_data["owner_email"]}
 
