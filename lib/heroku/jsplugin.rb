e9d4a695a71077addf13fbf3c08a96f610b3154e
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2015-05-07 19:32:19 -0700

    autofix v4 installs
    some users have unfortunately downloaded v4 clients that are not
    autoupdating. This will simply remove v4 if it is causing an issue.
    
    It will automatically be redownloaded if it is needed.

--- lib/heroku/jsplugin.rb
+++ lib/heroku/jsplugin.rb
@@ -72,6 +72,8 @@ class Heroku::JSPlugin
     commands_info['commands']
   rescue
     $stderr.puts "error loading plugin commands"
+    # Remove v4 if it is causing issues (for now)
+    File.delete(bin)
     return []
   end
 
