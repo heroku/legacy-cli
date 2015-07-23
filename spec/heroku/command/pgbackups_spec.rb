b90fda9891f0b1e97df4b2b3b83406d5617c9731
tef <tef@heroku.com>
2015-07-23 10:51:03 -0700

    Fix Spec

--- spec/heroku/command/pgbackups_spec.rb
+++ spec/heroku/command/pgbackups_spec.rb
@@ -6,8 +6,8 @@ module Heroku::Command
     it "does not list" do
         stderr, stdout = execute("pgbackups")
         expect(stderr).to eq <<-STDERR
- !    'heroku pgbackups' has been removed. 
- !    Please see 'heroku pg:backups' instead. 
+ !    'heroku pgbackups' has been removed.
+ !    Please see 'heroku pg:backups' instead.
  !    More Information: https://devcenter.heroku.com/articles/heroku-postgres-backups
 STDERR
         expect(stdout).to eq("")
