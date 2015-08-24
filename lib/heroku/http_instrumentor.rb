a6f2e4f6f89259f23f1fd8f0a1c10c18279a9983
dickeyxxx <jeff@dickeyxxx.com>
2015-08-24 15:08:27 -0700

    HEROKU_DEBUG improvments
    added arrows
    added request POST body output

--- lib/heroku/http_instrumentor.rb
+++ lib/heroku/http_instrumentor.rb
@@ -11,17 +11,18 @@ class HTTPInstrumentor
       when "excon.error"
         $stderr.puts params[:error].message
       when "excon.request"
-        $stderr.print "HTTP #{params[:method].upcase} #{params[:scheme]}://#{params[:host]}#{params[:path]} "
+        $stderr.print "--> HTTP #{params[:method].upcase} #{params[:scheme]}://#{params[:host]}#{params[:path]} "
         $stderr.print "[auth] " if headers['Authorization'] && headers['Authorization'] != 'Basic Og=='
         $stderr.print "[2fa] " if headers['Heroku-Two-Factor-Code']
         $stderr.puts filter(params[:query])
+        $stderr.puts "--> #{params[:body]}" if params[:body]
       when "excon.response"
-        $stderr.puts "#{params[:status]} #{params[:reason_phrase]}"
-        $stderr.puts "request-id: #{headers['Request-id']}" if headers['Request-Id']
+        $stderr.puts "<-- #{params[:status]} #{params[:reason_phrase]}"
+        $stderr.puts "<-- request-id: #{headers['Request-id']}" if headers['Request-Id']
         if headers['Content-Encoding'] == 'gzip'
-          $stderr.puts filter(ungzip(params[:body]))
+          $stderr.puts "<-- #{filter(ungzip(params[:body]))}"
         else
-          $stderr.puts filter(params[:body])
+          $stderr.puts "<-- #{filter(params[:body])}"
         end
       else
         $stderr.puts name
