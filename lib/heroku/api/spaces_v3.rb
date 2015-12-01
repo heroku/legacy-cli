019fe878dfa339c8cfaea594512e15ac84ed7449
Josh Sullivan <josh@dipperstove.com>
2015-12-01 13:30:59 -0800

    spaces: ensure we still request version 3
    Platform api requests that don't specify a version will default to v2.
    We want requests that return information about spaces or org apps to go
    to v3 instead

--- lib/heroku/api/spaces_v3.rb
+++ lib/heroku/api/spaces_v3.rb
@@ -1,12 +1,12 @@
 module Heroku
   class API
-    def get_space(space_identity)
+    def get_space_v3(space_identity)
       request(
         :method => :get,
         :expects => [200],
         :path => "/spaces/#{space_identity}",
         :headers => {
-          "Accept" => "application/vnd.heroku+json"
+          "Accept" => "application/vnd.heroku+json; version=3"
         }
       )
     end
