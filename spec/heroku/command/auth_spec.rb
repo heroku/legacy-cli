ec10f040619ccdf06e0b2eba3451019a140fca9f
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2016-07-31 13:43:14 -0700

    take out auth index spec

--- spec/heroku/command/auth_spec.rb
+++ spec/heroku/command/auth_spec.rb
@@ -2,19 +2,7 @@ require "spec_helper"
 require "heroku/command/auth"
 
 describe Heroku::Command::Auth do
-  describe "auth" do
-    it "displays heroku help auth" do
-      stderr, stdout = execute("auth")
-
-      expect(stderr).to eq("")
-      expect(stdout).to include "Additional commands"
-      expect(stdout).to include "auth:login"
-      expect(stdout).to include "auth:logout"
-    end
-  end
-
   describe "auth:token" do
-
     it "displays the user's api key" do
       stderr, stdout = execute("auth:token")
       expect(stderr).to eq("")
